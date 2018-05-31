106fecd7e48d044c7bcc7aa9b5a6b5d7371bd081
Ognyan Dimitrov <oddimitrov@gmail.com>
2018-05-31 19:47:31 +0300

    Solutions with C#

---
+++ T4. C# BASIC SYNTAX - EXERCISES/P03. MilesToKilometers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace P03._MilesToKilometers
+{
+    class Program
+    {
+        const double MILE_KM = 1.60934;
+        static void Main(string[] args)
+        {
+            double input = double.Parse(Console.ReadLine());
+            double convMileToKm = input * MILE_KM;
+
+            Console.WriteLine($"{convMileToKm:f2}");
+        }
+    }
+}
---
+++ T4. C# BASIC SYNTAX - EXERCISES/P04. BeverageLabels/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace P04._BeverageLabels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            double volume = double.Parse(Console.ReadLine());
+            double energy = double.Parse(Console.ReadLine());
+            double sugar = double.Parse(Console.ReadLine());
+
+            double energyVol = (volume * energy) / 100;
+            double sugarVol = (volume * sugar) / 100;
+
+            Console.WriteLine($"{volume}ml {name}:");
+            Console.WriteLine($"{energyVol}kcal, {sugarVol}g sugars");
+        }
+    }
+}
