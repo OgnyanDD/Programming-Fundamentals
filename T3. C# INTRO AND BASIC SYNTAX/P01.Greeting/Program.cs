0c7300cc6fc494851840570c31015a3124c78b44
Ognyan Dimitrov <oddimitrov@gmail.com>
2018-05-31 19:46:48 +0300

    Solutions with C#

---
+++ T3. C# INTRO AND BASIC SYNTAX/P01.Greeting/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace P01.Greeting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputName = Console.ReadLine();
+
+            Console.WriteLine($"Hello, {inputName}!");
+        }
+    }
+}
