55087c4274b96a59bee7df0e00a08d35e8b47c5a
Ognyan Dimitrov <oddimitrov@gmail.com>
2018-06-02 19:41:52 +0300

    Solutions with C#

---
+++ TF11. Arrays/P03.LastKNumbSumsSeq/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace P03.LastKNumbSumsSeq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+            long [] nums = new long[n];
+            nums[0] = 1;
+
+            for (int i = 1; i < n; i++)
+            {
+                long sum = 0;
+
+                for (int j = i - k; j <= i - 1; j++)
+                {
+                    if (j >= 0)
+                    {
+                        sum += nums[j];
+                    }
+                }
+
+                nums[i] = sum;
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"{nums[i]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ TF11. Arrays/P07.SumArrays/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+
+namespace P07.SumArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = Console.ReadLine()
+                       .Split()
+                       .Select(int.Parse)
+                       .ToArray();
+
+            int[] arr2 = Console.ReadLine()
+                       .Split()
+                       .Select(int.Parse)
+                       .ToArray();
+
+            if (arr1.Length <= arr2.Length)
+            {
+                int[] newArr2 = new int[arr2.Length];
+
+                for (int i = 0; i < newArr2.Length; i++)
+                {
+                    newArr2[i] = arr1[i % arr1.Length] + arr2[i % arr2.Length];
+
+                    if (i >= arr2.Length)
+                    {
+                        break;
+                    }
+                    Console.Write(newArr2[i] + " ");
+                }
+                Console.WriteLine();
+            }
+            else
+            {
+                int[] newArr1 = new int[arr1.Length];
+
+                for (int i = 0; i < newArr1.Length; i++)
+                {
+                    newArr1[i] = arr1[i % arr1.Length] + arr2[i % arr2.Length];
+
+                    if (i >= arr1.Length)
+                    {
+                        break;
+                    }
+                    Console.Write(newArr1[i] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
