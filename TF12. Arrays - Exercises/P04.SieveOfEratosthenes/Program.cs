75a1dda6cbd136fcc2f0e39346c4633d3a8d5938
Ognyan Dimitrov <oddimitrov@gmail.com>
2018-06-04 10:19:04 +0300

    Solutions with C#

---
+++ TF12. Arrays - Exercises/P04.SieveOfEratosthenes/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace P04.SieveOfEratosthenes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNumber = int.Parse(Console.ReadLine());
+
+            SieveOfEratosthenes(inputNumber);
+        }
+
+        private static void SieveOfEratosthenes(int n)
+        {
+            bool[] array = new bool[n + 1];
+
+            for (int i = 0; i <= n; i++)
+            {
+                array[i] = true;
+            }
+            array[0] = false;
+            array[1] = false;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == true)
+                {
+                    for (int a = 2; (a * i) <= n; a++)
+                    {
+                        array[a * i] = false;
+                    }
+                }
+            }
+            for (int j = 2; j <= n; j++)
+            {
+                if (array[j] == true)
+                {
+                    Console.Write(j + " ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
