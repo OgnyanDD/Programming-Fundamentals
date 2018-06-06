b03ce329e566d6aa27c4f39f48e28e86a3b4c44a
Ognyan Dimitrov <oddimitrov@gmail.com>
2018-06-06 07:32:25 +0300

    Solutions with C#

---
+++ TF13. LISTS/P01. RemNegAndRev/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01._RemNegAndRev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> input = Console.ReadLine()
+                                .Split()
+                                .Select(int.Parse)
+                                .ToList();
+
+            List<int> positiveNumbers = new List<int>();
+
+            for (int i = 0; i < input.Count; i++)
+            {
+                if (input[i] > 0)
+                {
+                    positiveNumbers.Add(input[i]);
+                }
+            }
+
+            positiveNumbers.Reverse();
+
+            if (positiveNumbers.Count > 0)
+            {
+                Console.WriteLine(string.Join(" ", positiveNumbers));
+            }
+            else
+            {
+                Console.WriteLine("empty");
+            }
+        }
+    }
+}
