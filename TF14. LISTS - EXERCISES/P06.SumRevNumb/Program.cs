a2f996be403e0b02bf9b66a0f0f9a42e8f304f7e
Ognyan Dimitrov <oddimitrov@gmail.com>
2018-06-07 21:10:13 +0300

    Solutions with C#

---
+++ TF14. LISTS - EXERCISES/P06.SumRevNumb/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace P06.SumRevNumb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                                .Split()
+                                .Select(int.Parse)
+                                .ToArray();
+
+            int sum = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                int rev = 0;
+                while (input[i] > 0)
+                {
+
+                    int digit = input[i] % 10;
+                    rev = rev * 10 + digit;
+                    input[i] = input[i] / 10;
+                }
+                sum += rev;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ TF14. LISTS - EXERCISES/P07.BombNumbers/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P07.BombNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> nums = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToList();
+
+            string[] intpit = Console.ReadLine().Split();
+
+            int number = int.Parse(intpit[0]);
+            int power = int.Parse(intpit[1]);
+
+            for (int i = 0; i < nums.Count; i++)
+            {
+                if (nums[i] == number)
+                {
+                    int left = Math.Max(i - power, 0);
+
+                    int right = Math.Min(i + power, nums.Count - 1);
+
+                    int lenght = right - left + 1;
+                    nums.RemoveRange(left, lenght);
+                    i = 0;
+                }
+            }
+
+            Console.WriteLine(nums.Sum());
+        }
+    }
+}
