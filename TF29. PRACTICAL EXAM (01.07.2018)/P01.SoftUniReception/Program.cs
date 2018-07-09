1530401ece190f7b508db0d3fd0cca03cc4bb521
Ognyan Dimitrov <oddimitrov@gmail.com>
2018-07-09 21:49:25 +0300

    My "lame" solution

---
+++ TF29. PRACTICAL EXAM (01.07.2018)/P01.SoftUniReception/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace P01.SoftUniReception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstEmp = int.Parse(Console.ReadLine());
+            int secondEmp = int.Parse(Console.ReadLine());
+            int thirdEmp = int.Parse(Console.ReadLine());
+
+            int students = int.Parse(Console.ReadLine());
+
+            int sumEmp = firstEmp + secondEmp + thirdEmp;
+
+            int counter = 0;
+
+            while (students > 0)
+            {
+                students -= sumEmp;
+                counter++;
+
+                if (counter % 4 == 0)
+                {
+                    students += sumEmp;
+                }
+            }
+            Console.WriteLine($"Time needed: {counter}h.");
+        }
+    }
+}
