5ac0b9856db5fa3b4ca756598e90f7206c79a31a
Ognyan Dimitrov <oddimitrov@gmail.com>
2018-08-18 17:29:23 +0300

    update

---
+++ TF26. EXAM PREPARATION I/01.Gaming Exam/P01.RageExpenses.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace P01.RageExpenses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lostGames = int.Parse(Console.ReadLine());
+
+            double headsetPrice = double.Parse(Console.ReadLine());
+            double mousePrice = double.Parse(Console.ReadLine());
+            double keyboardPrice = double.Parse(Console.ReadLine());
+            double displayPrice = double.Parse(Console.ReadLine());
+
+            double totalExpenses = 0;
+            int displayCount = 0;
+
+            for (int i = 1; i <= lostGames; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    totalExpenses += headsetPrice;
+                }
+
+                if (i % 3 == 0)
+                {
+                    totalExpenses += mousePrice;
+                }
+
+                if (i % 2 == 0 && i % 3 == 0)
+                {
+                    totalExpenses += keyboardPrice;
+                    displayCount++;
+
+                    if (displayCount % 2 == 0)
+                    {
+                        totalExpenses += displayPrice;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Rage expenses: {totalExpenses:f2} lv.");
+        }
+    }
+}
