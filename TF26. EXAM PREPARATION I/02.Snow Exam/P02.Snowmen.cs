c280b1f1d07ee1c7ae4e8e5e90af6d7448bdf966
Ognyan Dimitrov <oddimitrov@gmail.com>
2018-08-19 10:00:28 +0300

    update

---
+++ TF26. EXAM PREPARATION I/02.Snow Exam/P02.Snowmen.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P02.Snowmen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbersList = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+
+            while (numbersList.Count > 1)
+            {
+                List<int> snowmansToRemove = new List<int>();
+
+                for (int attacker = 0; attacker < numbersList.Count; attacker++)
+                {
+
+                    int target = numbersList[attacker] % numbersList.Count;
+
+                    int difference = Math.Abs(attacker - target);
+
+                    if (numbersList.Count - snowmansToRemove.Count == 1)
+                    {
+                        break;
+                    }
+
+                    if (snowmansToRemove.Contains(attacker))
+                    {
+                        continue;
+                    }
+
+                    if (difference == 0)
+                    {
+                        Console.WriteLine($"{attacker} performed harakiri");
+
+                        snowmansToRemove.Add(attacker);
+                        numbersList[attacker] = -1;
+                    }
+                    else if (difference % 2 == 0)
+                    {
+                        Console.WriteLine($"{attacker} x {target} -> {attacker} wins");
+
+                        numbersList[target] = -1;
+                        snowmansToRemove.Add(target);
+                    }
+                    else if (difference % 2 == 1)
+                    {
+                        Console.WriteLine($"{attacker} x {target} -> {target} wins");
+
+                        snowmansToRemove.Add(attacker);
+                        numbersList[attacker] = -1;
+                    }
+
+                    snowmansToRemove = snowmansToRemove.Distinct().ToList();
+                }
+
+                numbersList = numbersList.Where(x => x != -1).ToList();
+            }
+        }
+    }
+}
---
+++ TF26. EXAM PREPARATION I/02.Snow Exam/P04.Snowwhite.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P04.Snowwhite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+
+            Dictionary<string, List<Dwarf>> colorsDwarfs = new Dictionary<string, List<Dwarf>>();
+
+            List<Dwarf> allDwarfs = new List<Dwarf>();
+
+            while (command != "Once upon a time")
+            {
+                string[] dwarfInfo = command
+                    .Split(new char[] { ' ', '<', '>', ':' }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                string name = dwarfInfo[0];
+                string color = dwarfInfo[1];
+                int physics = int.Parse(dwarfInfo[2]);
+
+                if (!colorsDwarfs.ContainsKey(color))
+                {
+                    colorsDwarfs[color] = new List<Dwarf>();
+                }
+
+                List<Dwarf> dwarfsByCurrentCoulor = colorsDwarfs[color];
+                Dwarf foundDwarf = dwarfsByCurrentCoulor.FirstOrDefault(d => d.Name == name);
+
+                if (foundDwarf != null)
+                {
+                    foundDwarf.Physics = Math.Max(physics, foundDwarf.Physics);
+                }
+                else
+                {
+                    Dwarf dwarf = new Dwarf
+                    {
+                        Name = name,
+                        Color = color,
+                        Physics = physics
+                    };
+
+                    dwarfsByCurrentCoulor.Add(dwarf);
+                    allDwarfs.Add(dwarf);
+                }
+
+                command = Console.ReadLine();
+            }
+
+            List<Dwarf> result = allDwarfs.OrderByDescending(x => x.Physics).ThenByDescending(d => colorsDwarfs[d.Color].Count()).ToList();
+
+            foreach (var dwarf in result)
+            {
+                Console.WriteLine($"({dwarf.Color}) {dwarf.Name} <-> {dwarf.Physics}");
+            }
+        }
+    }
+
+    public class Dwarf
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public int Physics { get; set; }
+    }
+}
